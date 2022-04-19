70420601dad116e750c6330ac521175049340751
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-04-19 09:15:13 +0700

    add error handling

--- assets/scripts/fetch-app.js
+++ assets/scripts/fetch-app.js
@@ -14,26 +14,81 @@ function sendHttpRequest(method, url, body) {
     remember, then and catch will create a new promise
     return response.json we have to write it because we want to parse the
     JSON data into JavaScript array or JavaScript object.
+
+    second argument inside of fetch method is place where we do our configuration
+    to consume the data from API, basically we use the method and body property inside
+    of fetch method.
+    Special for body properties, we don't just pass the body itself, BUT IT HAS TO BE
+    PARSING THE DATA => JSON.stringify to convert our JavaScript array into JSON data type.
     */
-  return fetch(url).then((response) => {
-    return response.json();
-  });
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.status >= 200 && response.status < 300) {
+        return response.json();
+      } else {
+        return response.json().then((errorData) => {
+          console.log(errorData);
+          throw new Error("Something went wrong on our server!");
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      throw new Error("No internet connection");
+    });
 }
 
 async function fetchPosts() {
-  const responseData = await sendHttpRequest(
-    "GET",
-    "https://jsonplaceholder.typicode.com/posts"
-  );
-  const listOfPost = responseData;
-  listOfPost.forEach((post) => {
-    const postEl = document.importNode(postTemplate.content, true);
-    postEl.querySelector("h2").textContent = post.title.toUpperCase();
-    postEl.querySelector("p").textContent = post.body;
-    listElement.append(postEl);
-  });
+  try {
+    const responseData = await sendHttpRequest(
+      "GET",
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    const listOfPost = responseData;
+    listOfPost.forEach((post) => {
+      const postEl = document.importNode(postTemplate.content, true);
+      postEl.querySelector("h2").textContent = post.title.toUpperCase();
+      postEl.querySelector("p").textContent = post.body;
+      postEl.querySelector("li").id = post.id;
+      listElement.append(postEl);
+    });
+  } catch (error) {
+    alert(error.message);
+  }
 }
 
-async function createPost(title, content) {}
+async function createPost(title, content) {
+  const userId = Math.random();
+  const post = {
+    title: title,
+    body: content,
+    userId: userId,
+  };
+
+  sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", post);
+}
 
 fetchButton.addEventListener("click", fetchPosts);
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const enteredTitle = event.currentTarget.querySelector("#title").value;
+  const enteredBody = event.currentTarget.querySelector("#content").value;
+
+  createPost(enteredTitle, enteredBody);
+});
+postList.addEventListener("click", (event) => {
+  if (event.target.tagName === "BUTTON") {
+    const postId = event.target.closest("li").id;
+    sendHttpRequest(
+      "DELETE",
+      `https://jsonplaceholder.typicode.com/posts/${postId}`
+    );
+  }
+});
